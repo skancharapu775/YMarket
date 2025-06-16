20bc90ca40ee55696c2c8ffa9169b9c42b9010ac
Rico Chandra <ricochandra@Ricos-MacBook-Air-6.local>
2025-06-16 11:36:45 -0400

    Auth Backend 0.1

---
+++ backend/app/models.py
@@ -0,0 +1,16 @@
+# app/models.py
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DATABASE_URL = "sqlite:///./test.db"  # change to postgres later
+
+Base = declarative_base()
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)

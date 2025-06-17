12a45313a25fc3d1151e18ba5e750598c53f1053
sid.kancharapu@gmail.com <siddharth.kancharapu@icloud.com>
2025-06-17 00:28:32 -0400

    Update README.md, add seller dashboard/listing buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Navbar from './components/navbar.jsx'
+import Navbar from './components/Navbar.jsx'
 import './App.css'
 import Listing from './components/Listing.jsx'
 import {Routes, Route} from 'react-router-dom';
